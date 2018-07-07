3fb6f2480ef2f6bd4f017731ee484c0624f16ac8
Max de Vries <max.devries.1995@gmail.com>
2018-07-07 13:29:14 +0200

    Add checked subjects to course detail

--- opdracht2/public/js/script.js
+++ opdracht2/public/js/script.js
@@ -4,6 +4,7 @@
 
   var courses = {
     el: document.querySelectorAll('.course'),
+    all: [],
     init: function () {
       var self = this;
 
@@ -19,13 +20,50 @@
         arr.push(subject.dataset.subject);
       });
 
-      return {
+      var obj = {
         "course": course.dataset.course,
         "subjects": arr
       };
+
+      this.all.push(obj);
     }
   };
 
   courses.init();
+
+  var progress = {
+    el: document.querySelector('.qualification > progress')
+  };
+
+  var selection = {
+    el: document.querySelectorAll('.subjects > input[type="checkbox"]'),
+    init: function () {
+      var self = this;
+
+      this.el.forEach(function (subject) {
+        subject.addEventListener('change', function (e) {
+          if (this.checked) {
+            self.checkSubjects(this.value);
+          } else {
+            self.uncheckSubjects(this.value);
+          }
+        });
+      });
+    },
+    checkSubjects: function (val) {
+      var checkedSubjects = document.querySelectorAll('[data-subject="' + val + '"]');
+      checkedSubjects.forEach(function (subject) {
+        subject.classList.add('checked');
+      });
+    },
+    uncheckSubjects: function (val) {
+      var checkedSubjects = document.querySelectorAll('[data-subject="' + val + '"]');
+      checkedSubjects.forEach(function (subject) {
+        subject.classList.remove('checked');
+      });
+    }
+  };
+
+  selection.init();
 
 })();
