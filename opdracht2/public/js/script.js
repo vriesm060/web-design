7af71cf739e4eca299a3bcbf892adb8e249beead
Max de Vries <max.devries.1995@gmail.com>
2018-07-08 20:25:44 +0200

    Update tooltip

--- opdracht2/public/js/script.js
+++ opdracht2/public/js/script.js
@@ -59,43 +59,73 @@
       }
 
       this.renderPercentage(this.percentage);
-      this.changeTooltip(this.percentage);
+      this.renderTooltip(this.percentage);
     },
     renderPercentage: function (perc) {
       this.el.value = perc;
 
-      if (this.el.value >= 20 && this.el.value < 43) {
+      if (this.el.value >= 18 && this.el.value < 35) {
         this.el.dataset.zone = 'medium';
-      } else if (this.el.value >= 43) {
+      } else if (this.el.value >= 35) {
         this.el.dataset.zone = 'save';
       } else {
         this.el.dataset.zone = 'danger';
       }
+
+      if (this.el.value >= 75) {
+        this.el.classList.add('full');
+      } else {
+        this.el.classList.remove('full');
+      }
     },
-    changeTooltip: function (perc) {
+    renderTooltip: function (perc) {
+
+      console.log(perc + '%');
+
+      console.log(selection.selected);
+
+      var msg;
+
+      // Add tooltip texts for several cases:
+      // if (selection.selected.includes('html') || selection.selected.includes('css')) {
+      //   msg = 'Alleen HTML en/of CSS';
+      // } else if (selection.selected.includes('js') && selection.selected.length < 3) {
+      //   msg = 'Slechts JavaScript';
+      // }
+
+
       var progressbarLength = this.el.offsetWidth - 24;
       var tooltipLength = this.tooltip.offsetHeight;
-      var left = 'calc(-5.625rem + ' + Math.round((perc / 100) * progressbarLength) + 'px - (' + tooltipLength + 'px / 2))';
+      var left = 'calc(-5.625rem + ' + Math.round((perc / 75) * progressbarLength) + 'px - (' + tooltipLength + 'px / 2))';
       this.tooltip.style.left = left;
+
+      if (this.el.value >= 75) {
+        this.tooltip.style.left = 'calc(-5.625rem + ' + progressbarLength + 'px - (' + tooltipLength + 'px / 2))';
+      }
+
+      // this.tooltip.textContent = msg;
     }
   };
 
   var selection = {
     el: document.querySelectorAll('.subjects > input[type="checkbox"]'),
+    selected: [],
     init: function () {
       var self = this;
 
       document.querySelector('.subjects .action-button').setAttribute('disabled', true);
 
-      this.el.forEach(function (subject) {
+      this.el.forEach(function (subject, i) {
         subject.addEventListener('change', function (e) {
           if (this.checked) {
             document.querySelector('.qualification').classList.add('show');
             document.querySelector('.subjects .action-button').removeAttribute('disabled');
             self.checkSubjects(this.value);
+            self.selected.push(this.value);
             progress.calcPercentage(this.value, true);
           } else {
             self.uncheckSubjects(this.value);
+            self.selected.splice(self.selected.indexOf(this.value), 1);
             progress.calcPercentage(this.value, false);
           }
 
@@ -104,6 +134,11 @@
             document.querySelector('.qualification').classList.remove('show');
           }
         });
+      });
+
+      this.el[0].parentNode.addEventListener('submit', function (e) {
+        e.preventDefault();
+        window.location.hash = '#level';
       });
     },
     checkSubjects: function (val) {
